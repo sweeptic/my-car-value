ef67d42eb43aa065deac2357b1e82d226492d640
Attila Horvath <sweeptic@gmail.com>
2025-05-07 17:20:19 +0200

    01. Creating and Saving a User

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  //   repo: Repository<User>;
+
+  //   constructor(repo: Repository<User>) {
+  //     this.repo = repo;
+  //   }
+
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+}

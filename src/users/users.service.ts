ecbbd52c3ad3690d689485609a77bbc9b714f983
Attila Horvath <sweeptic@gmail.com>
2025-05-08 14:43:00 +0200

    07. Removing Users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -44,9 +44,12 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  remove(id: number) {
-    return this.repo.delete({
-      id: id,
-    });
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return this.repo.remove(user);
   }
 }

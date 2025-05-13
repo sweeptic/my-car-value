43b60f7646b60386ee5921401b57794cf7bddbd3
Attila Horvath <sweeptic@gmail.com>
2025-05-13 12:11:29 +0200

    20. Preventing Access with Authentication Guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Query,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -18,6 +19,7 @@ import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guards';
 
 @Serialize(UserDto)
 @Controller('auth')
@@ -43,6 +45,7 @@ export class UsersController {
   //   }
 
   @Get('/whoami')
+  @UseGuards(AuthGuard)
   whoAmI(@CurrentUser() user: User) {
     return user;
   }

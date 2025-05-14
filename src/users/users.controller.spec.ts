981c16d286ce493821b6cda752af25583330a151
Attila Horvath <sweeptic@gmail.com>
2025-05-14 17:30:32 +0200

    17. Unit Testing a Controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,10 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUserService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUserService = {
+      findOne: () => {},
+      find: () => {},
+      remove: () => {},
+      update: () => {},
+    };
+    fakeAuthService = {
+      signup: () => {},
+      signin: () => {},
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
     }).compile();
@@ -12,6 +27,7 @@ describe('UsersController', () => {
     controller = module.get<UsersController>(UsersController);
   });
 
+  //
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });

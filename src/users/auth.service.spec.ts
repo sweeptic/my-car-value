8c1e3fc15201c61331853a450832686db42fee92
Attila Horvath <sweeptic@gmail.com>
2025-05-14 16:17:18 +0200

    06. Improving File Layout #2

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,30 +3,31 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-let service;
+describe('AuthService', () => {
+  let service: AuthService;
 
-beforeEach(async () => {
-  // Create a fake copy of the users service
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+  beforeEach(async () => {
+    // Create a fake copy of the users service
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
 
-  service = module.get(AuthService);
-  //
-});
+    service = module.get(AuthService);
+  });
 
-it('can create an service of auth service', async () => {
-  expect(service).toBeDefined();
+  it('can create an service of auth service', async () => {
+    expect(service).toBeDefined();
+  });
 });

5e32ee16306ea9128c673a20409e667d41d607d3
Attila Horvath <sweeptic@gmail.com>
2025-05-14 17:54:48 +0200

    20. Not Super Effective Tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -47,9 +47,30 @@ describe('UsersController', () => {
 
     controller = module.get<UsersController>(UsersController);
   });
-  //
-  //
+
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUser('asd@asdf.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asd@asdf.com');
+  });
+
+  it('findOne returns a single user with the given id', async () => {
+    const users = await controller.findUser('1');
+    expect(users).toBeDefined();
+  });
+
+  it('findOne throws an error if the given id is not found', (done) => {
+    (async () => {
+      fakeUserService.findOne = () => null;
+      try {
+        await controller.findUser('1');
+      } catch (error) {
+        done();
+      }
+    })();
   });
 });

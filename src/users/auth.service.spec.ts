b0bbbc4b37aee3df004096776f411d45a0bf337e
Attila Horvath <sweeptic@gmail.com>
2025-05-14 16:54:48 +0200

    11. Testing the Signin Flow

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -5,10 +5,11 @@ import { User } from './user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // Create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -32,11 +33,34 @@ describe('AuthService', () => {
   });
 
   it('creates a new user with salted and hashed password', async () => {
-    const user = await service.signup('test@test.com', 'sigma');
+    const user = await service.signup('test@test.com', 'my-password');
 
-    expect(user.password).not.toEqual('sigma');
+    expect(user.password).not.toEqual('my-password');
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', (done) => {
+    (async () => {
+      fakeUsersService.find = () =>
+        Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+      try {
+        await service.signup('test@test.com', 'my-password');
+      } catch (error) {
+        console.log('error');
+        done();
+      }
+    })();
+  });
+
+  it('throws if signin is called with an unused email', (done) => {
+    (async () => {
+      try {
+        await service.signin('test@test.com', 'my-password');
+      } catch (error) {
+        done();
+      }
+    })();
   });
 });
